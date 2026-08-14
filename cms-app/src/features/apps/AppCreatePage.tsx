7a7e5cc555fd2626ed5545509565678bbf0d1987
Geraldo Borges <geraldo.estudos.trabalho@gmail.com>
2026-08-14 15:02:09 -0300

    Delete cms-app directory

--- cms-app/src/features/apps/AppCreatePage.tsx
+++
@@ -1,9 +0,0 @@
-function AppCreatePage() {
-  return (
-    <>
-      <h1>App Create Page</h1>
-    </>
-  );
-}
-
-export default AppCreatePage;
--- cms-app/src/features/templates/templateSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const templateSchema = z.object({
-  name: z.string().min(3, "Name is required"),
-});
-
-export type TemplateFormValues = z.infer<typeof templateSchema>;
